146dcc5b30ca20bd97d471eb9593d4f862a4a67a
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 21:18:16 +0300

    feat(routes): добавить authenticateToken в offerRoutes для защиты favorite toggle
    - Импортирована функция authenticateToken из authMiddleware
    - Добавлен middleware authenticateToken к роуту POST /favorite/:offerId/:status
    - Обеспечена защита маршрута изменения статуса избранного предложения

--- rental-service/server/routes/offerRoutes.js
+++ rental-service/server/routes/offerRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import {createOffer, getAllOffers, getFullOffer, getFavoriteOffers, toggleFavorite}  from '../controllers/offerController.js';
 import upload from '../middleware/upload.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
 router.get('/offers', getAllOffers);
 router.get('/offers/:id', getFullOffer);
 router.get('/favorite', getFavoriteOffers);
-router.post('/favorite/:offerId/:status', toggleFavorite);
+router.post('/favorite/:offerId/:status', authenticateToken, toggleFavorite);
 router.post('/offers', upload.fields([
     { name: 'previewImage', maxCount: 1},
     { name: 'photos', maxCount: 6 }
