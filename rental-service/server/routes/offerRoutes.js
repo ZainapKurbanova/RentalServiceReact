2079e922e393d4d026eb5132d0c963c66f613385
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 15:34:45 +0300

    feat(offer-routes): добавить маршрут создания предложения с загрузкой изображений
    - Добавлен POST /offers с middleware upload для загрузки previewImage и photos
    - Подключены контроллеры createOffer и getAllOffers
    - Файл routes/offerRoutes.js обновлён

--- rental-service/server/routes/offerRoutes.js
+++ rental-service/server/routes/offerRoutes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
-import {getAllOffers} from '../controllers/offerController.js';
+import {createOffer, getAllOffers} from '../controllers/offerController.js';
+import upload from '../middleware/upload.js';
 
 const router = Router();
 
 router.get('/offers', getAllOffers);
+router.post('/offers', upload.fields([
+    { name: 'previewImage', maxCount: 1},
+    { name: 'photos', maxCount: 6 }
+]), createOffer);
+
 export default router;
