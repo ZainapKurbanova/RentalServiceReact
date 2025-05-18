9121f2207d3ce5325e265875fb2f17e84e08192c
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 20:55:33 +0300

    feat(review): добавлена функция getReviewsByOfferId для получения отзывов по офферу
    - Получает список отзывов по offerId с загрузкой данных автора (username, avatar, userType)
    - Сортирует отзывы по дате публикации в порядке убывания
    - Преобразует данные отзывов с помощью адаптера adaptReviewToClient
    - Возвращает массив адаптированных комментариев в формате JSON
    - В случае ошибки возвращает 500 с сообщением об ошибке

--- rental-service/server/controllers/reviewController.js
+++ rental-service/server/controllers/reviewController.js
@@ -1,5 +1,6 @@
 import {Review} from '../models/review.js';
 import ApiError from '../error/ApiError.js';
+import { adaptReviewToClient } from '../adapters/reviewAdapter.js';
 import { User } from '../models/user.js';
 const addReview = async (req, res, next) => {
     try {
@@ -22,4 +23,19 @@ const addReview = async (req, res, next) => {
         next(ApiError.badRequest('Ошибка при добавлении комментария'));
     }
 };
-export {addReview};
+
+const getReviewsByOfferId = async (req, res, next) => {
+    try {
+        const reviews = await Review.findAll({
+            where: { OfferId: req.params.offerId },
+            include: { model: User, as: 'author' },
+            order: [['publishDate', 'DESC']]
+        });
+        const adaptedReviews = reviews.map(adaptReviewToClient);
+        res.json(adaptedReviews);
+    } catch (error) {
+        console.error(error);
+        next(ApiError.internal('Ошибка при получении комментариев'));
+    }
+};
+export {addReview, getReviewsByOfferId};
