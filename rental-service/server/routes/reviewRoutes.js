ddd75f73527050e50c6e2143e590e4b0abb46c89
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 21:19:32 +0300

    feat(routes): добавить authenticateToken в reviewRoutes для защиты добавления отзывов
    - Импортирована функция authenticateToken из authMiddleware
    - Добавлен middleware authenticateToken к роуту POST /:offerId для добавления отзывов
    - Обеспечена аутентификация пользователей при создании отзывов

--- rental-service/server/routes/reviewRoutes.js
+++ rental-service/server/routes/reviewRoutes.js
@@ -1,9 +1,9 @@
 import Router from 'express';
 import { addReview, getReviewsByOfferId } from '../controllers/reviewController.js';
-
+import { authenticateToken } from '../middleware/authMiddleware.js';
 const router = new Router();
 
 router.get('/:offerId', getReviewsByOfferId);
-router.post('/:offerId',  addReview);
+router.post('/:offerId',  authenticateToken, addReview);
 
 export default router;
