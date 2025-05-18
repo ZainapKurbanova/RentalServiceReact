5cf563fad4fd098defbf2a6492a381cfbc85a252
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 15:58:52 +0300

    feat(server): добавлена раздача статических изображений из папки /static
    - Настроен middleware express.static для доступа к изображениям по пути /static

--- rental-service/server/index.js
+++ rental-service/server/index.js
@@ -5,24 +5,31 @@ import sequelize from './config/database.js';
 // import { Offer } from './models/offer.js';
 // import { Review } from './models/review.js';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import router from './routes/index.js';
-
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
 dotenv.config();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors()); 
 app.use(express.json()); 
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
 app.use('/', router);
+app.use(errorMiddleware);
+
 const start = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
-        app.listen(PORT, () => console.log(Сервер запущен на порте ${PORT}));
+        app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
     } catch (e) {
         console.log(e);
     }
 }
 
 start();
+
