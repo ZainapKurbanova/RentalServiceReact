710919d9590587f017ba9e76ad9f64b63a1416f2
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 21:23:16 +0300

    добавить маршруты GET /login и DELETE /logout
    - Добавлен защищённый маршрут GET /login с middleware authenticateToken и контроллером checkAuth для проверки авторизации
    - Добавлен маршрут DELETE /logout для выхода пользователя
    - Импортированы необходимые контроллеры и middleware

--- rental-service/server/routes/userRoutes.js
+++ rental-service/server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import upload from '../middleware/upload.js';
-import { registration, login } from '../controllers/userController.js';
-
+import { registration, login, checkAuth, logout } from '../controllers/userController.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
 const router = new Router();
 
 router.post('/register', upload.single('avatar'), registration);
 router.post('/login', login);
+router.get('/login', authenticateToken, checkAuth);
+router.delete('/logout', logout);
 export default router;
