e0021529a4d82fb427f7638ea678d8a44f9d3981
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 15:29:11 +0300

    chore(routes): подключить userRoutes в главный роутер
    - Импортирован userRoutes из './userRoutes.js'
    - Добавлен router.use('/', userRoutes) для обработки маршрутов пользователей
    - Сохранилась маршрутизация offerRoutes
    - Файл routes/index.js обновлён

--- rental-service/server/routes/index.js
+++ rental-service/server/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import offerRoutes from './offerRoutes.js'; 
-
+import userRoutes from './userRoutes.js';
 const router = Router();
 
 router.use('/', offerRoutes);
+router.use('/', userRoutes);
 
 export default router;
