6fe7c08f6e51190156be06ca8ac8804dc8cf08b5
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 20:51:58 +0300

    feat(routes): зарегистрированы маршруты для офферов, пользователей и отзывов
    - Подключены offerRoutes, userRoutes и reviewRoutes
    - Все маршруты объединены в одном корневом маршрутизаторе
    - Маршруты offer и user зарегистрированы по корневому пути '/'
    - Маршруты отзывов подключены по пути '/comments'

--- rental-service/server/routes/index.js
+++ rental-service/server/routes/index.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import offerRoutes from './offerRoutes.js'; 
 import userRoutes from './userRoutes.js';
+import reviewRouter from './reviewRoutes.js';
+
 const router = Router();
 
 router.use('/', offerRoutes);
 router.use('/', userRoutes);
+router.use('/comments', reviewRouter);
 
-export default router;
+export {router};
