1369dbaf4b527953254f7192da8a26e912f2cb54
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-18 21:03:01 +0300

    feat(offer): добавлены маршруты для получения и изменения избранных предложений
    - Добавлен маршрут GET /favorite, возвращающий список всех предложений с isFavorite = true
    - Добавлен маршрут POST /favorite/:offerId/:status для добавления или удаления предложения из избранного
    - Маршрут POST принимает offerId и статус (1 — добавить в избранное, 0 — удалить)
    - Оба маршрута подключены в файле offerRoutes.js

--- rental-service/server/routes/offerRoutes.js
+++ rental-service/server/routes/offerRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import {createOffer, getAllOffers} from '../controllers/offerController.js';
+import {createOffer, getAllOffers, getFullOffer, getFavoriteOffers, toggleFavorite}  from '../controllers/offerController.js';
 import upload from '../middleware/upload.js';
 
 const router = Router();
 
 router.get('/offers', getAllOffers);
+router.get('/offers/:id', getFullOffer);
+router.get('/favorite', getFavoriteOffers);
+router.post('/favorite/:offerId/:status', toggleFavorite);
 router.post('/offers', upload.fields([
     { name: 'previewImage', maxCount: 1},
     { name: 'photos', maxCount: 6 }
