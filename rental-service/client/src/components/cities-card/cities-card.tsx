5b8580e991fd45ea1eed7d1b44bfd253ad0f47ce
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-29 15:09:10 +0300

    feat(cities-card): добавить обработку событий наведения в CitiesCard
    В компонент CitiesCard добавлена функциональность обработки событий наведения:
    - Импортирован хук useState для управления состоянием offerId.
    - Добавлены обработчики onMouseOver и onMouseOut для установки и сброса offerId при наведении на карточку.
    - Ссылка на страницу предложения обновлена с использованием AppRoute.Offer для динамического формирования URL.

--- rental-service/client/src/components/cities-card/cities-card.tsx
+++ rental-service/client/src/components/cities-card/cities-card.tsx
@@ -1,24 +1,41 @@
-function CitiesCard() {
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import { useState } from 'react';
+
+type CitiesCardProps = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  isPremium: boolean;
+  previewImage: string;
+  rating: number;
+};
+
+function CitiesCard({ id, title, type, price, previewImage, isPremium, rating }: CitiesCardProps) {
+  const [,setOfferId] = useState('');
   return (
-    <article className="cities__card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
+    <article className="cities__card place-card" onMouseOver={()=>setOfferId(id)} onMouseOut={() => setOfferId('')}>
+      {isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
+        <Link to={`${AppRoute.Offer}/${id}`}>
           <img
             className="place-card__image"
-            src="img/apartment-03.jpg"
+            src={previewImage}
             width="260"
             height="200"
             alt="Place image"
           />
-        </a>
+        </Link>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">€180</b>
+            <b className="place-card__price-value">€{price}</b>
             <span className="place-card__price-text">/ night</span>
           </div>
           <button
@@ -37,17 +54,17 @@ function CitiesCard() {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '100%' }}></span>
+            <span style={{ width: `${rating * 20}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Nice, cozy, warm big bed apartment</a>
+          <Link to={`/offer/${id}`}>{title}</Link>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
-};
+}
 
 export { CitiesCard };
