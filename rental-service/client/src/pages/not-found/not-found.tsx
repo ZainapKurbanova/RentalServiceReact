e1624d622bac1035c92bd86f7e6c0a38e72d836c
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-29 15:35:31 +0300

    feat(not-found-page): интегрировать компонент Logo
    - Импортирован компонент Logo из ../../components/logo/logo.
    - Статическая разметка логотипа в header__left заменена на компонент <Logo/>.

--- rental-service/client/src/pages/not-found/not-found.tsx
+++ rental-service/client/src/pages/not-found/not-found.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "../../components/logo/logo";
 function NotFoundPage() {
   return (
     <div className="page">
@@ -5,15 +6,7 @@ function NotFoundPage() {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="/">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="Rent service logo"
-                  width="81"
-                  height="41"
-                />
-              </a>
+              <Logo/>
             </div>
           </div>
         </div>
