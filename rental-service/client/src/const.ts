a8cca710d7c3356b8491bed6de1bbaf5f1f5628f
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-06-14 11:22:25 +0300

    Update const.ts

--- rental-service/client/src/const.ts
+++ rental-service/client/src/const.ts
@@ -1,17 +1,80 @@
-const AppRoute = {
-    Main: '/',
-    Login: '/login',
-    Favorites: '/favourites',
-    Offer: '/offer/:id',
+import { CityOffer } from "./types/offer";
+
+ export const Setting = {
+  RentalOffersCount: 4,
+};
+export const AppRoute = {
+  Main: '/',
+  Login: '/login',
+  Favorites: '/favorites',
+  Offer: '/offer/:id',
+  NotFound: '*',
 } as const;
 
-const AuthorizationStatus = {
-    Auth: 'AUTH',
-    NoAuth: 'NO_AUTH',
-    Unknown: 'UNKNOWN',
-}
-const Setting = {
-    rentOffersCount: 312,
-}
+export const AuthorizationStatus = {
+  Auth: 'AUTH',
+  NoAuth: 'NO_AUTH',
+  Unknown: 'UNKNOWN',
+} as const;
 
-export {AppRoute, AuthorizationStatus, Setting};
+export const URL_MARKER_DEFAULT = '/img/pin.svg';
+export const URL_MARKER_CURRENT = '/img/pin-active.svg';
+
+
+export const CITIES_LOCATION : CityOffer[] = [
+ {
+   name: 'Paris',
+   location: {
+     latitude: 48.5112,
+     longitude: 2.2055,
+     zoom: 8
+   }
+ },
+ {
+   name: 'Cologne',
+   location: {
+     latitude: 50.9375,
+     longitude: 6.9603,
+     zoom: 8
+   }
+ },
+ {
+   name: 'Brussels',
+   location: {
+     latitude: 50.8503,
+     longitude: 4.3517,
+     zoom: 8
+   }
+ },
+ {
+   name: 'Amsterdam',
+   location: {
+     latitude: 52.2226,
+     longitude: 4.5322,
+     zoom: 8
+   }
+ },
+ {
+   name: 'Hamburg',
+   location: {
+     latitude: 53.5511,
+     longitude: 9.9937,
+     zoom: 8
+   }
+ },
+ {
+   name: 'Dusseldorf',
+   location: {
+     latitude: 51.2277,
+     longitude: 6.7735,
+     zoom: 8
+   }
+ },
+];
+
+export const SortOffersType = {
+  Popular: 'Popular',
+  PriceToHigh: 'Price: low to high',
+  PriceToLow: 'Price: high to low',
+  TopRated: 'Top rated first',
+};
